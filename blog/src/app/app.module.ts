715185462fa79129d29a40a5fe6bf8aff7d64b3e
Luiz Eduardo Santana Leite <luiz.eduardo1@ibm.com>
2023-12-26 11:18:39 -0300

    Subindo Blog

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderTitleComponent } from './components/header-title/header-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeaderTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Caramelo decide dar um passseio",
+        "description": "Caramelo cansado de ser enganado e não ser levado para passear decide pegar o primeiro busão e andar sem direção pelo mundo",
+        "photoCover": "../assets/img/noticiaPrincipal.webp"
+    },
+    {
+        "id": "2",
+        "title": "Feira de adoção de cães",
+        "description": "Na proxima semana tem inicio da feira de adoção de cães que ocorrerá no centro da cidade a partir das 10hrs, não fique de fora!!!",
+        "photoCover": "../assets/img/card1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Nota Caramelo",
+        "description": "Foi decretado oficialmente, após muita luta Caramelo teve seu reconhecimento com sua imagem estampada na nota de 200",
+        "photoCover": "../assets/img/card2.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Cão Artilheiro",
+        "description": "Na partida entre Cruzeiro x PSG todos foram surpreendidos por uma invasão inusitada, Fred invadiu o campo e tendo feito um hat-trick levou a bola do jogo para casa",
+        "photoCover": "../assets/img/card3.jpg"
+    },
+]
